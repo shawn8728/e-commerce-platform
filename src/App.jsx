1bebef0f54052c3bc84aea7381b01d3c6ed8c1b1
shawn8728 <wang61107@gmail.com>
2024-04-02 13:49:43 -0700

    Add page header

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import '@/assets/css/App.css'
 
 import Home from '@/pages/Home'
 import ProductDetail from '@/components/ProductDetail'
+import Header from '@/components/Header'
 import { CartProvider } from '@/context/CartContext'
 
 function App() {
   return (
     <CartProvider>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="product">
