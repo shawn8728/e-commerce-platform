02856acf5e6f4ff6bc89f9c3928672006821a4aa
shawn8728 <wang61107@gmail.com>
2024-04-03 01:41:00 -0700

    Disable checkout button

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -174,12 +174,13 @@ function Cart() {
                         Shipping and taxes calculated at checkout.
                       </p>
                       <div className="mt-6">
-                        <a
-                          href="#"
-                          className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                        <button
+                          disabled
+                          className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700
+                            opacity-50 cursor-not-allowed"
                         >
                           Checkout
-                        </a>
+                        </button>
                       </div>
                       <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                         <p>
